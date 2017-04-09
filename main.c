534e03b73e33d327d866bc2a146af872152e732d
Ailén González <ailengonzalezp@yahoo.com.ar>
2017-04-09 16:46:30 -0300

    update: soporta -i - -o -

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int show_version(){
     printf("Gonzalez Perez, Ailen\t\tPadron: 97043\n");
     printf("Mariotti, Maria Eugenia\t\tPadron: 96260\n");
     printf("Raña, Cristian Ezequiel\t\tPadron: 95457\n \n");
-
+    
     return EXIT_SUCCESS;
 
 }
@@ -51,60 +51,6 @@ int show_help(){
 
 }
 
-<<<<<<< HEAD:Sunday/main.c
-char * d_reader(FILE* file){
-
-	//I initialize a "basic" string
-	char* read = malloc(sizeof(char)*MAX);
-
-	char buffer[MAX];
-	int totalCharacters = 0;
-	int readCharacters = fread(buffer, sizeof(char), MAX, file);
-
-	while(readCharacters > 0){
-		//If I read a character, I copy it into
-		char* appendix = &read[totalCharacters];
-		strcpy(appendix, buffer);
-		read = (char *) realloc(read, sizeof(read) + 1);
-		totalCharacters += readCharacters;
-		readCharacters = fread(file, sizeof(char), MAX,file);
-	}
-  
-	fclose(file);
-	return read;
-}
-
-char * e_reader(FILE* file){
-
-	//I initialize a "basic" string
-	char* read = malloc(sizeof(char)*MAX);
-
-	char buffer[MAX];
-	int totalCharacters = 0;
-	int readCharacters = fread(buffer, sizeof(char), MAX, file);
-
-	while(readCharacters > 0){
-		//If I read a character, I copy it into
-		char* appendix = &read[totalCharacters];
-		strcpy(appendix, buffer);
-		read = (char *) realloc(read, sizeof(read) + 1);
-		totalCharacters += readCharacters;
-		readCharacters = fread(file, sizeof(char), MAX,file);
-	}
-	//Eliminar \0
-
-	for(int i = 0; i < strlen(read);i++){
-		if(read[i] == '\n'){
-			for(int j = i; j < strlen(read);j++)
-				read[j] = read[j+1];
-		}
-	}
-
-	fclose(file);
-
-	return read;
-
-=======
 char * reader(FILE* miFile){
 	
 	long lSize;
@@ -132,7 +78,6 @@ char * reader(FILE* miFile){
 	
 	return buffer;
 	
->>>>>>> 1611d346371fc1352675dbb356ea0baee750d0cb:main.c
 }
 
 
@@ -140,22 +85,22 @@ char * b64_decode(char *src, size_t len) {
   int i = 0; //Contadores
   int j = 0; //para los
   int l = 0; //recorridos
-
+  
   size_t size = 0;
   char *dec = NULL;
   unsigned char buf[3];
   unsigned char tmp[4];
 
   // alloc
-  dec = (char *) malloc(0);
+  dec = (char *) malloc(0); 
   if (NULL == dec) { return NULL; }
 
   //Vamos a tomar caracteres mientras no se termine src
   while (len--) {
     //Si el caracter no pertenece a los posibles en b64
     //o si es el símbolo = -> rompe el ciclo
-    if ( ('=' == src[j]) || (!(isalnum(src[j]) || '+' == src[j] || '/' == src[j])) ) {
-		break;
+    if ( ('=' == src[j]) || (!(isalnum(src[j]) || '+' == src[j] || '/' == src[j])) ) { 
+		break; 
 	}
 
     //Vamos a ir leyendo hasta 4 bytes (los 4 bytes de b64 nos dan 3 bytes de ascii
@@ -178,15 +123,15 @@ char * b64_decode(char *src, size_t len) {
 
       //Teniendo los números asociados se hace la decodificación en sí misma
       //relacionando los valores en b64 y ascii
-      //A considerar: 0x30 = 48 0x3c = 60  0xf= 15 0x3 = 3
+      //A considerar: 0x30 = 48 0x3c = 60  0xf= 15 0x3 = 3 
       //Se encuentran explicados los cálculos en el informe
-
-
+       
+      
       buf[0] = (tmp[0] << 2) + ((tmp[1] & 0x30) >> 4); //Línea 1
       buf[1] = ((tmp[1] & 0xf) << 4) + ((tmp[2] & 0x3c) >> 2); //Línea 2
       buf[2] = ((tmp[2] & 0x3) << 6) + tmp[3]; //Línea 3
 
-      //Guardamos lo obtenido
+      //Guardamos lo obtenido 
       dec = (char *) realloc(dec, size + 3);
       for (i = 0; i < 3; ++i) {
         dec[size++] = buf[i];
@@ -229,7 +174,7 @@ char * b64_decode(char *src, size_t len) {
   //Agregamos el caracter de fin de línea.
   dec = (char *) realloc(dec, size + 1);
   dec[size] = '\0';
-
+ 
   return dec;
 }
 
@@ -254,17 +199,17 @@ char * b64_encode (char *src, size_t len) {
       buf[1] = ((tmp[0] & 0x03) << 4) + ((tmp[1] & 0xf0) >> 4);
       buf[2] = ((tmp[1] & 0x0f) << 2) + ((tmp[2] & 0xc0) >> 6);
       buf[3] = tmp[2] & 0x3f;
-
+    
       enc = (char *) realloc(enc, size + 4);
       for (i = 0; i < 4; ++i) {
         enc[size++] = b64_table[buf[i]];
       }
 
-      i = 0;
+      i = 0; 
     }
   }
 
-
+ 
   if (i > 0) {
     for (j = i; j < 3; ++j) {
       tmp[j] = '\0';
@@ -280,7 +225,7 @@ char * b64_encode (char *src, size_t len) {
       enc[size++] = b64_table[buf[j]];
     }
 
-
+    
     while ((i++ < 3)) {
       enc = (char *) realloc(enc, size + 1);
       enc[size++] = '=';
@@ -295,67 +240,65 @@ char * b64_encode (char *src, size_t len) {
 }
 
 int main(int argc, char* argv[]){
-
+		
 		FILE* input = stdin; //Where do we get the input?
 		FILE* output = stdout; //Where do we get the output?
 		char* action = "encode"; //What we are going to do
 		bool showVersion = false; //To check if the version option was used
 		bool showHelp = false;
-
-		for (int i=1;i<argc;i++){
+				
+		for (int i=1;i<argc;i++){ 
 			if (strcmp("-h",argv[i])==0 || strcmp("--help",argv[i])==0 ){ //Help -> show help and continue execution
 				show_help();
 				showHelp = true;
 				break;
 			}
-
-			if (strcmp("-V",argv[i])==0 || strcmp("--version",argv[i])==0 ){ //Version -> show version and save the boolean
+			
+			if (strcmp("-V",argv[i])==0 || strcmp("--version",argv[i])==0 ){ //Version -> show version and save the boolean 
 				show_version();
 				showVersion = true;
 				break;
 			}
-
+		
 			if (strcmp("-i",argv[i])==0 || strcmp("--input",argv[i])==0 ){ //If -i option is set, we have an input file
-				input = fopen(argv[i+1],"rb+");
-				if (input==NULL){
-					printf("Error de apertura de archivo de entrada. No se puede continuar.\n");
-					return -1;
+				if (strcmp("-",argv[i+1])!= 0){
+					input = fopen(argv[i+1],"rb+");
+					if (input==NULL){
+						printf("Error de apertura de archivo de entrada. No se puede continuar.\n");
+						return -1;
+					}
 				}
 				i++;
 			}
-
+		
 			if (strcmp("-o",argv[i])==0 || strcmp("--output",argv[i])==0 ){ //If -o option is set, we have an output file
+				if (strcmp("-",argv[i+1])!= 0){
 					output = fopen(argv[i+1],"wb+");
 					if (output==NULL){
-					printf("Error de apertura de archivo de salida. No se puede continuar.\n");
-					return -1;
+						printf("Error de apertura de archivo de salida. No se puede continuar.\n");
+						return -1;
+					}
 				}
 				i++;
 			}
-
+		
 			if (strcmp("-a",argv[i])==0 || strcmp("--action",argv[i])==0 ){ //Defines if we decode or encode
 				action = argv[i+1];
 				if (strcmp("decode",action) !=0 && strcmp("encode",action) !=0){
 					printf("%s no es una opción válida.\n",action);
 					return -1;
-				}
+				}  
 				i++;
 			}
 		}
-
+		
 		if (showVersion || showHelp){ //-v and -h "show and quit" options
 			if (input != stdin)
 				fclose(input);
 			if (output != stdout)
 				fclose(output);
-			return 0;
+			return 0; 
 		}
-<<<<<<< HEAD:Sunday/main.c
-
-
-
-
-=======
 		
 		char * target;
 		
@@ -364,17 +307,16 @@ int main(int argc, char* argv[]){
 		else
 			target = reader(input); //Argument for b64_decode/encode
 		
->>>>>>> 1611d346371fc1352675dbb356ea0baee750d0cb:main.c
 		char* result; //Where we save the decoded/encoded string
-
+		
 		if ( strcmp("decode",action) ==0 ){ //If action = decode -> call b64_decode
 			result = b64_decode(target,strlen(target));
 		}
-
+		
 		else{ //If action = encode -> call b64_encode
 			result = b64_encode(target,strlen(target));
 		}
-
+		
 		if(output == stdout) //If stdout -> printf the result
 			printf("\n%s\n",result); //Make it friendly
 
@@ -382,16 +324,11 @@ int main(int argc, char* argv[]){
 			fprintf(output, "%s", result);
 			fclose(output); //Close the output file
 		}
-
+		
 		if(input != stdin)
 			fclose(input); //If it was open, close the input file
-<<<<<<< HEAD:Sunday/main.c
-
-		//free(result);
-=======
 			
 		free(result);  
 		
->>>>>>> 1611d346371fc1352675dbb356ea0baee750d0cb:main.c
 		return 0; //Everything went fine
 }
